cbcf06fe6deea05da9f31ac7b0dd2ffa21dd4777
yuvarajkannan17 <yuvarajkannan17@gmail.com>
2024-09-13 11:37:49 +0530

    uploading new project to githib

---
+++ info.js
@@ -0,0 +1,22 @@
+
+
+//  let name="yeswanth";
+//  let city="hyderabad";
+//  let email="yeswanth@gmail.com"
+
+//  export {name as myName,city as c,email};
+
+let colors=["red","white","orange"];
+
+export default colors;
+
+
+let city=["chennai","hyderabad"];
+
+export { city};
+
+// object and function
+// named export
+// default export
+
+
---
+++ script.js
@@ -0,0 +1,20 @@
+// Module
+// import and export
+// varible,object,array,function
+
+// Named Export
+// Default Export
+
+// import {myName,c,email} from './info.js'
+
+// console.log(myName);
+// console.log(c);
+// console.log(email);
+
+import co from "./info.js";
+
+console.log(co);
+
+import {city} from './info.js'
+
+console.log(city);
